0b14731da4312d2ee02f0d9a4f7206650b5475d7
RomanZemlin <roman.zemlin@mail.ru>
2023-06-22 19:34:58 +0300

    Дописан __init__, calculate_total_price, apply_discount, main отрабатывает

--- src/item.py
+++ src/item.py
@@ -13,7 +13,12 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self.name)
+        self.all.append(self.price)
+        self.all.append(self.quantity)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
